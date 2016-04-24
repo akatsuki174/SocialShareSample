9fd7e55e4cefcbc7d09658f379af5617e12cc607
akatsuki174 <dev2015js@gmail.com>
2016-04-24 22:20:48 +0900

    issue #2 シェアするためのボタンを設置

--- SocialShareSample/ViewController.swift
+++ SocialShareSample/ViewController.swift
@@ -9,10 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private let button = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        button.frame = CGRectMake(0,0,180,50)
+        button.backgroundColor = UIColor.blueColor()
+        button.layer.masksToBounds = true
+        button.setTitle("Share!", forState: .Normal)
+        button.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+        button.layer.cornerRadius = 15.0
+        let frame = self.view.frame
+        button.layer.position = CGPoint(x: frame.width / 2, y:frame.height / 2)
+        button.addTarget(self, action: #selector(ViewController.tappedButton(_:)), forControlEvents: .TouchUpInside)
+        self.view.addSubview(button)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func tappedButton(sender: UIButton) {
+        
+    }
 }
 
